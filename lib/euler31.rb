270f69de29cf9f3d83197b8832bef774efea7034
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-03-22 23:43:42 -0500

    performance tweaks

--- lib/euler31.rb
+++ lib/euler31.rb
@@ -31,33 +31,31 @@ module Euler31
     else
       candidates = coins.select { |coin| value - coin >= 0 }
 
-      exact_change = candidates.bsearch { |coin| coin == value }
+      key2 = [candidates, value]
 
-      exact_change_array = if exact_change
-                           [Multiset.new([exact_change])]
-                         else
-                           []
-                         end
+      if @memo.has_key?(key2)
+        @memo[key2]
+      else
+        exact_change, remaining_candidates = candidates.partition { |coin| coin == value }
 
-      exact_change_set = Set.new(exact_change_array)
+        exact_change_array = if exact_change.length > 0
+                             [Multiset.new([exact_change.first])]
+                           else
+                             []
+                           end
 
-      remaining_candidates = candidates - [exact_change]
+        non_trivial_factors = Set.new
 
-      non_trivial_factors = remaining_candidates.collect do |candidate|
-        ch = change(remaining_candidates, value - candidate)
+        remaining_candidates.each do |candidate|
+          ch = change(remaining_candidates, value - candidate)
 
-        if ch.size == 0
-          nil
-        else
-          ch.collect { |factors| factors.dup << candidate }.uniq
+          ch.each do |factors|
+            non_trivial_factors << (factors.dup << candidate)
+          end
         end
+
+        @memo[key2] = @memo[key] = Set.new(exact_change_array) | non_trivial_factors
       end
-
-      non_nil_non_trivial_factors = non_trivial_factors.flatten.compact
-
-      unique_non_nil_non_trivial_factors = Set.new(non_nil_non_trivial_factors)
-
-      @memo[key] = exact_change_set | unique_non_nil_non_trivial_factors
     end
   end
 end
