2766e642dfcbe5fd48ff699481816859eafc0232
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 17:54:01 -0600

    update editorconfig

--- lib/euler31.rb
+++ lib/euler31.rb
@@ -7,54 +7,54 @@ require_relative 'cli'
 # Project Euler #31
 #
 module Euler31
-  ENGLISH_COINS = [
-    1,
-    2,
-    5,
-    10,
-    20,
-    50,
-    100,
-    200
-  ].freeze
+    ENGLISH_COINS = [
+        1,
+        2,
+        5,
+        10,
+        20,
+        50,
+        100,
+        200
+    ].freeze
 
-  attr_accessor :memo
+    attr_accessor :memo
 
-  @memo = {}
+    @memo = {}
 
-  def self._change(coins, value)
-    exact_change, candidates = coins.partition { |coin| coin == value }
+    def self._change(coins, value)
+        exact_change, candidates = coins.partition { |coin| coin == value }
 
-    leftover_candidates = candidates.select { |coin| coin <= value }
+        leftover_candidates = candidates.select { |coin| coin <= value }
 
 
-    exact_change_array = if exact_change.length > 0
-                         [Multiset.new([exact_change.first])]
-                       else
-                         []
-                       end
+        exact_change_array = if exact_change.length > 0
+            [Multiset.new([exact_change.first])]
+        else
+            []
+        end
 
-    non_trivial_factors = Set.new
+        non_trivial_factors = Set.new
 
-    leftover_candidates.each do |candidate|
-      diff = value - candidate
-      remaining_candidates = leftover_candidates.select { |c| c <= diff }
-      ch = change(remaining_candidates, diff)
+        leftover_candidates.each do |candidate|
+            diff = value - candidate
+            remaining_candidates = leftover_candidates.select { |c| c <= diff }
+            ch = change(remaining_candidates, diff)
 
-      ch.each do |factors|
-        non_trivial_factors << (factors.dup << candidate)
-      end
+            ch.each do |factors|
+                non_trivial_factors << (factors.dup << candidate)
+            end
+        end
+
+        Set.new(exact_change_array) | non_trivial_factors
     end
 
-    Set.new(exact_change_array) | non_trivial_factors
-  end
-
-  # Memoized
-  def self.change(coins, value)
-    if @memo.has_key?(value)
-      @memo[value]
-    else
-      @memo[value] = _change(coins, value)
+    # Memoized
+    def self.change(coins, value)
+        if @memo.has_key?(value)
+            @memo[value]
+        else
+            @memo[value] = _change(coins, value)
+        end
     end
-  end
 end
